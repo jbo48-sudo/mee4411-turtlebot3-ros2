7073f2d9a89a44973775f56fbef87d28e2e8bb91
Philip Dames <pdames@temple.edu>
2025-09-26 10:26:51 -0400

    Update visualization code to also publish grid lines for the map

--- occupancy_grid/occupancy_grid/block_visualizer.py
+++ occupancy_grid/occupancy_grid/block_visualizer.py
@@ -4,8 +4,11 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
+from geometry_msgs.msg import Point
 from std_msgs.msg import Header
 from visualization_msgs.msg import Marker, MarkerArray
+
+from numpy import arange, ceil
 
 
 class BlockVisualizer(Node):
@@ -16,6 +19,8 @@ class BlockVisualizer(Node):
         # initialize publisher
         latching_qos = QoSProfile(depth=1, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
         blocks_pub = self.create_publisher(MarkerArray, 'blocks', qos_profile=latching_qos)
+        grid_pub = self.create_publisher(Marker, 'grid_lines', qos_profile=latching_qos)
+        # https://docs.ros2.org/foxy/api/nav_msgs/msg/GridCells.html
 
         # initialize parameters
         self.declare_parameter('boundary',
@@ -30,12 +35,16 @@ class BlockVisualizer(Node):
         self.declare_parameter('alpha', 0.5,
                                descriptor=ParameterDescriptor(
                                    type=ParameterType.PARAMETER_DOUBLE))
+        self.declare_parameter('resolution',
+                               descriptor=ParameterDescriptor(
+                                   type=ParameterType.PARAMETER_DOUBLE))
 
         # get parameter values
         boundary = self.get_parameter('boundary').get_parameter_value().double_array_value
         blocks = self.get_parameter('blocks').get_parameter_value().double_array_value
         blocks = [blocks[i:i+4] for i in range(0, len(blocks), 4)]  # reshape the block array
         alpha = self.get_parameter('alpha').get_parameter_value().double_value
+        resolution = self.get_parameter('resolution').get_parameter_value().double_value
 
         # print map information to screen
         self.get_logger().info("boundary: {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(*boundary))
@@ -104,6 +113,35 @@ class BlockVisualizer(Node):
             ma.append(m)
 
         blocks_pub.publish(MarkerArray(markers=ma))
+
+        # Create GridCells message
+        m = Marker()
+        m.header = h
+        m.ns = "grid_lines"
+        m.id = i
+        m.type = Marker.LINE_LIST
+        m.action = Marker.ADD
+        m.pose.orientation.w = 1.
+        m.scale.x = 0.01  # line width
+        m.color.r = 128./255.
+        m.color.g = 128./255.
+        m.color.b = 128./255.
+        m.color.a = 1.0
+
+        # Populate the grid cells
+        xmin = boundary[0]
+        xmax = boundary[0]+ceil((boundary[2]-boundary[0])/resolution)*resolution
+        ymin = boundary[1]
+        ymax = boundary[1]+ceil((boundary[3]-boundary[1])/resolution)*resolution
+        for x in arange(xmin, xmax+1.e-6, resolution):
+            m.points.append(Point(x=x, y=ymin, z=0.0))
+            m.points.append(Point(x=x, y=ymax, z=0.0))
+        for y in arange(ymin, ymax+1.e-6, resolution):
+            m.points.append(Point(x=xmin, y=y, z=0.0))
+            m.points.append(Point(x=xmax, y=y, z=0.0))
+
+        # Publish the GridCells message
+        grid_pub.publish(m)
 
 
 def main(args=None):
